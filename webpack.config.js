46520483d46ca4b9af902f3aea735544a0a3682b
Geovana Silva <sts.geovana@gmail.com>
2020-04-27 00:50:59 -0300

    fix: update filename output at webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
-    filename: "bundle.js",
+    filename: "dist/bundle.js",
   },
   module: {
     rules: [
