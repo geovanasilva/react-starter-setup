3ed6dad2368c86c5aaf0fa995b1308673820b811
Geovana Silva <sts.geovana@gmail.com>
2020-04-27 00:51:41 -0300

    feat: create basic files
    * Create index.html
    * Create app.js
    * Create styles.scss

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+const app = <p>React, Webpack and Babel</p>;
+
+ReactDOM.render(app, document.getElementById('root'));
